f68beb0473f5748611e7de80d9d1aa6f504cf083
AkhiJain <vanakundreakhilesh@gmail.com>
2026-07-05 21:36:49 +0530

    subnets are created and applied

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,33 +1,67 @@
 terraform {
 
-required_providers {
+  required_providers {
 
-aws = {
+    aws = {
 
-source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
 
-version = "~> 5.0"
+    }
 
-}
-
-}
+  }
 
 }
 
 
 provider "aws" {
 
-region = "ap-south-1"
+  region = "ap-south-1"
 
 }
 
-module "vpc"{
+
+module "vpc" {
+
+  source = "../../modules/vpc"
 
 
-source = "../../modules/vpc"
+  vpc_cidr = "10.0.0.0/16"
 
 
-vpc_cidr = "10.0.0.0/16"
+  public_subnets = [
 
+    "10.0.1.0/24",
+
+    "10.0.2.0/24"
+
+  ]
+
+
+  private_app_subnets = [
+
+    "10.0.11.0/24",
+
+    "10.0.12.0/24"
+
+  ]
+
+
+  private_db_subnets = [
+
+    "10.0.21.0/24",
+
+    "10.0.22.0/24"
+
+  ]
+
+
+  azs = [
+
+    "ap-south-1a",
+
+    "ap-south-1b"
+
+  ]
 
 }
